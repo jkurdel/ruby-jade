4d86ed4c99bed67a9f1aa578b33609795b758035
Ryan Fitzgerald <rwfitzge@gmail.com>
2012-12-12 15:33:50 -0800

    Initial working version

--- lib/ruby-jade.rb
+++ lib/ruby-jade.rb
@@ -1,7 +1,26 @@
 require "ruby-jade/version"
+require "execjs"
 
-module Ruby
-  module Jade
-    # Your code goes here...
+module Jade
+  class << self
+    def path
+      File.expand_path '../ruby-jade/jade.min.js', __FILE__
+    end
+
+    def context
+      @context ||= begin
+        ExecJS.compile <<-JS
+          var window = {};
+          #{File.read(path)}
+          var compile = function (template, options) {
+            return window.jade.compile(template, options).toString();
+          }
+        JS
+      end
+    end
+
+    def compile(template, options = {:client => true})
+      context.call('compile', template, options)
+    end
   end
 end
